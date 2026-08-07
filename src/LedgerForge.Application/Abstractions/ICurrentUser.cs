ce09b71505596d433aff9db45fe2b69e7be12332
SherNoir <161985106+Sher-Noir@users.noreply.github.com>
2026-08-07 11:58:04 -0400

    Move current-user abstraction into LedgerForge namespace

---
+++ src/LedgerForge.Application/Abstractions/ICurrentUser.cs
@@ -0,0 +1,9 @@
+namespace LedgerForge.Application.Abstractions;
+
+public interface ICurrentUser
+{
+    bool IsAuthenticated { get; }
+    string DomainIdentity { get; }
+    string DisplayName { get; }
+    IReadOnlyCollection<string> Roles { get; }
+}
